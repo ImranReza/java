4e953d55178bd37898007e46b061172d1dacb8c4
ImranReza <ImranReza.cse@gmail.com>
2024-07-27 16:14:44 +0600

    Star pattern program add

---
+++ StarPatternOne.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class StarPatternOne {
+    public static void main(String[] args) {
+        Scanner inpuScanner = new Scanner(System.in);
+
+        System.out.println("Enter A Number : ");
+        
+        int n = inpuScanner.nextInt();
+
+        //logic 
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.err.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
